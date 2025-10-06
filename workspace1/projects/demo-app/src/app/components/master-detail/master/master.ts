9dff66d95bbc814cf2020e691fbf679ccde1d5cf
Francisco de Borja Cabeza Rozas <borja.cabeza@cabodigital.es>
2025-10-06 16:35:01 +0200

    Modificaciones durante la clase

--- workspace1/projects/demo-app/src/app/components/master-detail/master/master.ts
+++ workspace1/projects/demo-app/src/app/components/master-detail/master/master.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { Detail } from '../detail/detail';
+
+import data from '../../../data/usersData.json';
+import { User } from '../../../models/user.model';
+
 
 @Component({
   selector: 'app-master',
@@ -7,5 +12,31 @@ import { Component } from '@angular/core';
   styleUrl: './master.css'
 })
 export class Master {
+  usersList: Array<User>;
+  selectUser: User;
+  // Dos formas de usar ViewChild para obtener la referencia del componente hijo
+  // 1. Usando la clase del componente
+  // 2. Usando una referencia de template (ver master.html)
+  @ViewChild(Detail) detailComponent1!: Detail;
+  @ViewChild('detalle') detailComponent2!: Detail;
+
+  constructor() {
+    this.usersList = data;
+    this.selectUser = new User();
+  } 
+
+  onSelectUser(user: User) {
+    this.selectUser = user;
+
+    // Alternativa para pasar el usuario seleccionado al componente hijo
+    // Usando ViewChild
+    // this.detailComponent1.usuarioHijo = user;
+    // this.detailComponent1.onSave();
+  }
+
+  onUsuarioSaveEmirt(user: User) {
+    console.log('Usuario salvado: ', user);
+    alert('Usuario salvado: ' + user.firstname + ' ' + user.lastname);
+  } 
 
 }
